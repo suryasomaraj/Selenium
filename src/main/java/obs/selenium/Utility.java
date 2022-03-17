0afa08e8b306571c0c3b51ed69b2f47d2c0a4814
suryasomaraj <suryasomaraj94@gmail.com>
2022-03-17 20:37:31 +0530

    utility

--- src/main/java/obs/selenium/Utility.java
+++ src/main/java/obs/selenium/Utility.java
@@ -1,2 +1,30 @@
-package obs.selenium;public class Utility {
+package obs.selenium;
+
+public class Utility {
+
+    public static String randomString(int n)
+    {
+      //  String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"+ "0123456789"+ "abcdefghijklmnopqrstuvxyz";
+        String AlphaNumericString ="suryasomaraj";
+
+        StringBuilder sb = new StringBuilder(n);
+        for (int i = 0; i < n; i++) {
+            int index = (int) (AlphaNumericString.length()
+                    * Math.random());
+            sb.append(AlphaNumericString.charAt(index));
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args)
+    {
+
+        int n =10;
+        System.out.println(Utility.randomString(n));
+    }
 }
+
+
+
+
