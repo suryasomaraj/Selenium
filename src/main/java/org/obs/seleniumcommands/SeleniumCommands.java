6ca922a1b2e1e90d683d237cc76bcbf10f280129
suryasomaraj <suryasomaraj94@gmail.com>
2022-03-28 08:24:55 +0530

    commands

--- src/main/java/org/obs/seleniumcommands/SeleniumCommands.java
+++ src/main/java/org/obs/seleniumcommands/SeleniumCommands.java
@@ -5,13 +5,17 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import javax.swing.*;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
 public class SeleniumCommands {
     WebDriver driver;
@@ -45,7 +49,6 @@ public class SeleniumCommands {
     public void tearDown(){
         driver.close();
     }
-
    @Test
     public void verifyHomepageTitle(){
         driver.get("http://demowebshop.tricentis.com");
@@ -53,7 +56,6 @@ public class SeleniumCommands {
         String expectedTitle="Demo Web Shop";
         Assert.assertEquals(actualTitle,expectedTitle,"invalid Page Title");
     }
-
     @Test
     public void verifyLogin(){
         driver.get("http://demowebshop.tricentis.com");
@@ -108,7 +110,11 @@ public class SeleniumCommands {
         String actualloginbuttontext=submit.getAttribute("value");
         System.out.println("actual login button text= "+actualloginbuttontext);
         String expectedloginbuttontext="Log in";
-        Assert.assertEquals(actualloginbuttontext,expectedloginbuttontext,"incorrect text in login button");
+       // Assert.assertEquals(actualloginbuttontext,expectedloginbuttontext,"incorrect text in login button");
+        String tagname=submit.getTagName();
+        System.out.println("Tagname= "+tagname);
+        String cssproperty=submit.getCssValue("color");
+        System.out.println("cssproperty= "+cssproperty);
     }
     @Test
     public void searchButton(){
@@ -150,14 +156,138 @@ public class SeleniumCommands {
         cpassword.sendKeys("qwerty@123");
         WebElement registerbutton=driver.findElement(By.cssSelector("input[name='register-button']"));
         registerbutton.click();
+        //a[text()='suryasomaraj13@gmail.com']
     }
     public void selectGender(String gender){
         List<WebElement> radio=driver.findElements(By.xpath("//label[@class='forcheckbox']"));
         for(int i=0;i< radio.size();i++){
-            if(radio.get(i).getText().equals("Female")) {
+            if(radio.get(i).getText().equals(gender)) {
                 radio.get(i).click();
+            }else{
+                System.out.println("Invalid");
             }
         }
+    }
+    @Test
+    public void verifyElementPresent(){
+        driver.get("http://demowebshop.tricentis.com");
+        WebElement login=driver.findElement(By.cssSelector("li>a.ico-login"));
+        login.click();
+        WebElement submit=driver.findElement(By.cssSelector("input[value='Log in']"));
+        boolean result=submit.isDisplayed();
+        System.out.println("Result="+result);
+        Assert.assertTrue(result,"Submit button not displayed");
+    }
+    @Test
+    public void verifyElementEnabled(){
+        driver.get("http://demowebshop.tricentis.com");
+        WebElement login=driver.findElement(By.cssSelector("li>a.ico-login"));
+        login.click();
+        WebElement submit=driver.findElement(By.cssSelector("input[value='Log in']"));
+        boolean enabledStatus=submit.isEnabled();
+        System.out.println("enabledStatus= "+enabledStatus);
+        Assert.assertTrue(enabledStatus,"Submit button is not enabled");
+    }
+    @Test
+    public void verifyCheckBoxSelectionStatus(){
+        driver.get("http://demowebshop.tricentis.com");
+        WebElement login=driver.findElement(By.cssSelector("li>a.ico-login"));
+        login.click();
+        WebElement checkbox=driver.findElement(By.cssSelector("input[type='checkbox']"));
+        boolean selectionStatusBeforeClick=checkbox.isSelected();
+        System.out.println(selectionStatusBeforeClick);
+        Assert.assertFalse(selectionStatusBeforeClick,"Checkbox selection is not expected");
+        checkbox.click();
+        boolean selectionStatus=checkbox.isSelected();
+        System.out.println("SelectionStatus= "+selectionStatus);
+        Assert.assertTrue(selectionStatus,"Checkbox is not selected");
+    }
+    @Test
+    public void verifyPromptAlert(){
+        driver.get("https://demoqa.com/alerts");
+        WebElement prompt=driver.findElement(By.cssSelector("button#promtButton"));
+        prompt.click();
+        Alert alert=driver.switchTo().alert();
+        String alertString=alert.getText();
+        System.out.println(alertString);
+        alert.sendKeys("surya");
+        alert.accept();
+        //alert.dismiss();
+    }
+    @Test
+    public void verifyDropDown(){
+        driver.get("https://demo.guru99.com/test/newtours/");
+        List<WebElement> registerLink=driver.findElements(By.xpath("//td[@class='mouseOut']/child::a"));
+        for(int i=0;i< registerLink.size();i++) {
+            if (registerLink.get(i).getText().equals("REGISTER")) {
+                registerLink.get(i).click();
+                break;
+            }
+        }
+        WebElement dropdown=driver.findElement(By.xpath("//select[@name='country']"));
+        Select select=new Select(dropdown);
+        //select.selectByVisibleText("INDIA");
+        select.selectByValue("INDIA");
+        select.selectByIndex(20);
+       List<WebElement> dropdownValues=select.getOptions();
+        System.out.println(dropdownValues.size());
+        for (int i=0;i<dropdownValues.size();i++) {
+            System.out.println(dropdownValues.get(i).getText());
+        }
+    }
+    @Test
+    public void verifySimpleAlert(){
+        driver.get("https://demoqa.com/alerts");
+        WebElement simpleAlert=driver.findElement(By.cssSelector("button#alertButton"));
+        simpleAlert.click();
+        Alert alert=driver.switchTo().alert();
+        alert.accept();
+    }
+    @Test
+    public void verifyConfirmationAlert(){
+        driver.get("https://demoqa.com/alerts");
+        WebElement confirmationAlert=driver.findElement(By.cssSelector("button#confirmButton"));
+        confirmationAlert.click();
+        Alert alert=driver.switchTo().alert();
+        //alert.accept();
+        alert.dismiss();
+    }
+    @Test
+    public void verifyDeleteCustomer(){
+        driver.get("https://demo.guru99.com/test/delete_customer.php");
+        WebElement customerTextBox=driver.findElement(By.cssSelector("td>input[name='cusid']"));
+        customerTextBox.sendKeys("Surya");
+        WebElement submitButton=driver.findElement(By.xpath("//input[@name='submit']"));
+        submitButton.click();
+        Alert alert=driver.switchTo().alert();
+        alert.accept();
+        alert.accept();
+    }
+    @Test
+    public void verifyMultipleWindows() {
+        driver.get("https://demo.guru99.com/popup.php");
+        String parentWindow = driver.getWindowHandle();
+        System.out.println("parentWindow= " + parentWindow);
+        WebElement clickHere = driver.findElement(By.cssSelector("p>a[target='_blank']"));
+        clickHere.click();
+        Set<String> windows = driver.getWindowHandles();
+        System.out.println("number of windows =" + windows.size());
+        Iterator<String> ite = windows.iterator();
+        while (ite.hasNext()) {
+            System.out.println(ite.next());
+            String newWindow = ite.next();
+            if (!newWindow.equals(parentWindow)) {
+                driver.switchTo().window(newWindow);
+                Utility utility = new Utility();
+                String mail = Utility.random();
+                WebElement emailId = driver.findElement(By.cssSelector("td>input[name='emailid']"));
+                emailId.sendKeys(mail);
+                WebElement submitButton = driver.findElement(By.cssSelector("input[name='btnLogin']"));
+                submitButton.click();
+                driver.close();
+            }
+        }
+        driver.switchTo().window(parentWindow);
     }
 }
 
